1c93a021de9de7e851f145cffcd9e185052e6d4d
Matthew Love <matthewlove94@gmail.com>
2025-03-31 20:22:29 +0100

    Can construct from an array now

--- Containers/span.hpp
+++ Containers/span.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
 #include <cstddef>
+#include <type_traits>
+#include <concepts>
 
 namespace ml {
+template <typename T>
+concept has_data_and_size = requires (T t) {
+
+    { t.size() } -> std::same_as<std::size_t>;
+    requires std::is_pointer_v<decltype(t.data())>;
+};
+
 template <typename T>
 class span {
 public:
@@ -10,12 +19,25 @@ public:
     using ptr_t = T *;
     using size_t = std::size_t;
 private:
-    ptr_t ptr;
-    size_t size;
-private:
+    ptr_t ptr_;
+    size_t size_;
+public:
     span() = delete;
-    span(ptr_t ptr_, size_t size_)
-        : ptr{ptr_}
-        , size{size_} {}
+    span(ptr_t ptr, size_t size)
+        : ptr_{ptr}
+        , size_{size} {}
+    template <typename U>
+        requires has_data_and_size<U>
+    span(U & container)
+        : ptr_{container.data()}
+        , size_{container.size()} {}
+
+    auto size() const -> size_t {
+        return size_;
+    }
 };
+
+template <typename T>
+    requires has_data_and_size<T>
+span(T& iter) -> span<std::remove_cvref_t<decltype(*iter.data())>>;
 }
---
+++ tests/test_span.cpp
@@ -0,0 +1,23 @@
+#include <array>
+#include <cstddef>
+
+#include <gtest/gtest.h>
+
+#include "Containers/span.hpp"
+
+using size_t = ml::span<int>::size_t;
+
+TEST(span, ptr_size_ctor) {
+    int * ptr{nullptr};
+    constexpr size_t size{1};
+
+    auto span{ml::span(ptr, size)};
+
+    ASSERT_EQ(size, span.size());
+}
+
+TEST(span, from_array) {
+    std::array<int, 5> values{0};
+    
+    auto span{ml::span(values)};
+}
