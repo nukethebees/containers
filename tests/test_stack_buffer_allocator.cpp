99c9486e0a13c3a66f2927a9340869deaa4d338e
Matthew Love <matthewlove94@gmail.com>
2025-04-05 14:20:13 +0100

    Added test for overflow

--- tests/test_stack_buffer_allocator.cpp
+++ tests/test_stack_buffer_allocator.cpp
@@ -24,3 +24,13 @@ TEST(stack_buffer_allocator, vector_push_back) {
     vec.push_back(5);
     ASSERT_EQ(vec.size(), 5);
 }
+
+TEST(stack_buffer_allocator, vector_push_back_overflow) {
+    int_vec<5> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(5);
+    ASSERT_THROW(vec.push_back(6), std::bad_alloc);
+}
