f7518d6b138d0bb8f0f9ed6da34a84f317e7dc57
MatthewLoveADI <matthew.love@analog.com>
2025-04-16 14:48:53 +0100

    Added benchmark to look at multipool performance

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -5,6 +5,7 @@
 
 #include "containers/arena_memory_resource.hpp"
 #include "containers/stack_buffer_memory_resource.hpp"
+#include "containers/multi_arena_resource.hpp"
 
 constexpr int n_placements{25000};
 constexpr int n_reserve{10};
@@ -12,7 +13,7 @@ constexpr int n_repetitions{5};
 
 constexpr int arena_elems_to_allocate{10'000};
 
-static void BM_vector_alloc_std(benchmark::State & state) {
+static void BM_vector_alloc_std(benchmark::State& state) {
     for (auto _ : state) {
         std::vector<std::vector<char>> c_vecs;
 
@@ -24,7 +25,7 @@ static void BM_vector_alloc_std(benchmark::State & state) {
     }
     state.SetItemsProcessed(state.iterations() * n_placements);
 }
-static void BM_vector_alloc_arena(benchmark::State & state) {
+static void BM_vector_alloc_arena(benchmark::State& state) {
     for (auto _ : state) {
         ml::ArenaMemoryResource char_resource{sizeof(char) * arena_elems_to_allocate};
         ml::ArenaAllocator<char> c_alloc{&char_resource};
@@ -39,7 +40,7 @@ static void BM_vector_alloc_arena(benchmark::State & state) {
     state.SetItemsProcessed(state.iterations() * n_placements);
 }
 
-static void BM_vector_alloc_arena_pmr(benchmark::State & state) {
+static void BM_vector_alloc_arena_pmr(benchmark::State& state) {
     for (auto _ : state) {
         ml::ArenaMemoryResourcePmr char_resource{sizeof(char) * arena_elems_to_allocate};
         std::vector<std::pmr::vector<char>> c_vecs;
@@ -54,7 +55,7 @@ static void BM_vector_alloc_arena_pmr(benchmark::State & state) {
 }
 
 // Comparison benchmark vs the buffer version
-static void BM_vec_stackbuf_new_ref(benchmark::State & state) {
+static void BM_vec_stackbuf_new_ref(benchmark::State& state) {
     static constexpr std::size_t n_ints{50};
 
     for (auto _ : state) {
@@ -66,7 +67,7 @@ static void BM_vec_stackbuf_new_ref(benchmark::State & state) {
     }
     state.SetItemsProcessed(state.iterations());
 }
-static void BM_vec_stackbuf(benchmark::State & state) {
+static void BM_vec_stackbuf(benchmark::State& state) {
     static constexpr std::size_t n_ints{50};
     using config = ml::StackAllocConfig<int, n_ints>;
 
@@ -82,11 +83,67 @@ static void BM_vec_stackbuf(benchmark::State & state) {
     state.SetItemsProcessed(state.iterations());
 }
 
-//BENCHMARK(BM_vector_alloc_std)->Repetitions(n_repetitions);
+static void BM_mixed_vector_std(benchmark::State& state) {
+    for (auto _ : state) {
+        std::vector<char> char_vec{};
+        std::vector<double> double_vec{};
+        std::vector<std::string> string_vec{};
 
-BENCHMARK(BM_vector_alloc_arena);
-BENCHMARK(BM_vector_alloc_arena_pmr);
+        for (int i = 0; i < n_placements; ++i) {
+            char_vec.push_back(static_cast<char>(i % 256));
+            double_vec.push_back(i * 1.1);
+            string_vec.emplace_back(50, 'a' + (i % 26));
+        }
+    }
+    state.SetItemsProcessed(state.iterations() * n_placements * 3);
+}
+static void BM_mixed_vector_single_resource(benchmark::State& state) {
+    for (auto _ : state) {
+        ml::ArenaMemoryResourcePmr resource{sizeof(char) * arena_elems_to_allocate};
+        std::pmr::polymorphic_allocator<char> char_alloc{&resource};
+        std::pmr::polymorphic_allocator<double> double_alloc{&resource};
+        std::pmr::polymorphic_allocator<std::string> string_alloc{&resource};
 
-//BENCHMARK(BM_vec_stackbuf_new_ref)->Repetitions(n_repetitions);
-//BENCHMARK(BM_vec_stackbuf)->Repetitions(n_repetitions);
+        std::pmr::vector<char> char_vec{char_alloc};
+        std::pmr::vector<double> double_vec{double_alloc};
+        std::pmr::vector<std::string> string_vec{string_alloc};
+
+        for (int i = 0; i < n_placements; ++i) {
+            char_vec.push_back(static_cast<char>(i % 256));
+            double_vec.push_back(i * 1.1);
+            string_vec.emplace_back(50, 'a' + (i % 26));
+        }
+    }
+    state.SetItemsProcessed(state.iterations() * n_placements * 3);
+}
+static void BM_mixed_vector_multipool_allocator(benchmark::State& state) {
+    for (auto _ : state) {
+        constexpr std::size_t n_resources = 3;
+        ml::MultiArenaMemoryResourcePmr resource{n_resources, sizeof(char) * arena_elems_to_allocate};
+
+        auto* char_arena = resource.get_resource(0);
+        auto* double_arena = resource.get_resource(1);
+        auto* string_arena = resource.get_resource(2);
+
+        std::pmr::polymorphic_allocator<char> char_alloc{char_arena};
+        std::pmr::polymorphic_allocator<double> double_alloc{double_arena};
+        std::pmr::polymorphic_allocator<std::string> string_alloc{string_arena};
+
+        std::pmr::vector<char> char_vec{char_alloc};
+        std::pmr::vector<double> double_vec{double_alloc};
+        std::pmr::vector<std::pmr::string> string_vec{string_alloc};
+
+        for (int i = 0; i < n_placements; ++i) {
+            char_vec.push_back(static_cast<char>(i % 256));
+            double_vec.push_back(i * 1.1);
+            string_vec.emplace_back(50, 'a' + (i % 26));
+        }
+    }
+    state.SetItemsProcessed(state.iterations() * n_placements * 3);
+}
+
+BENCHMARK(BM_mixed_vector_std);
+BENCHMARK(BM_mixed_vector_single_resource);
+BENCHMARK(BM_mixed_vector_multipool_allocator);
+
 BENCHMARK_MAIN();
