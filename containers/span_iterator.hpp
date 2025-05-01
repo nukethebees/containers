1d0286b843e711fcc0e7fda1b606b5644a1b6f3e
Matthew Love <matthewlove94@gmail.com>
2025-05-01 09:48:33 +0100

    ptr to ptr_

--- containers/span_iterator.hpp
+++ containers/span_iterator.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
+#include <cstddef>
+#include <iterator>
+
 namespace ml {
 template <typename T>
 class span_iterator {
-  private:
-    T* ptr{nullptr};
   public:
     using difference_type = std::ptrdiff_t;
     using value_type = T;
@@ -16,18 +17,24 @@ class span_iterator {
     // Needed to satisfy the default constructible requirement
     span_iterator() = default;
     explicit span_iterator(T* ptr)
-        : ptr(ptr) {}
+        : ptr_(ptr) {}
+
+    span_iterator(span_iterator const&) = default;
+    span_iterator(span_iterator&&) = default;
+
+    auto operator=(span_iterator const&) -> span_iterator& = default;
+    auto operator=(span_iterator&&) -> span_iterator& = default;
 
     // Dereferencing
     // For some reason, a const iterator needs to return T& to meet the iter requirements
-    auto operator*() const -> reference { return *ptr; }
-    auto operator*() -> reference { return *ptr; }
-    auto operator->() const -> T* { return ptr; }
-    auto operator[](difference_type n) const -> reference { return *(ptr + n); }
+    auto operator*() const -> reference { return *ptr_; }
+    auto operator*() -> reference { return *ptr_; }
+    auto operator->() const -> T* { return ptr_; }
+    auto operator[](difference_type n) const -> reference { return *(ptr_ + n); }
 
     // Increment / decrement
     auto operator++() -> span_iterator& {
-        ++ptr;
+        ++ptr_;
         return *this;
     }
     auto operator++(int) -> span_iterator {
@@ -37,7 +44,7 @@ class span_iterator {
     }
 
     auto operator--() -> span_iterator& {
-        --ptr;
+        --ptr_;
         return *this;
     }
     auto operator--(int) -> span_iterator {
@@ -47,28 +54,30 @@ class span_iterator {
     }
 
     // Math operators
-    auto operator+(difference_type n) const -> span_iterator { return span_iterator(ptr + n); }
+    auto operator+(difference_type n) const -> span_iterator { return span_iterator(ptr_ + n); }
     friend auto operator+(difference_type lhs, span_iterator const& rhs) -> span_iterator {
-        return span_iterator(lhs + rhs.ptr);
+        return span_iterator(lhs + rhs.ptr_);
     }
-    auto operator-(difference_type n) const -> span_iterator { return span_iterator(ptr - n); }
+    auto operator-(difference_type n) const -> span_iterator { return span_iterator(ptr_ - n); }
     friend auto operator-(difference_type lhs, span_iterator const& rhs) -> span_iterator {
-        return span_iterator(lhs - rhs.ptr);
+        return span_iterator(lhs - rhs.ptr_);
     }
-    auto operator-(span_iterator const& n) const -> difference_type { return difference_type{ptr - n.ptr}; }
+    auto operator-(span_iterator const& n) const -> difference_type { return difference_type{ptr_ - n.ptr_}; }
     auto operator+=(difference_type n) -> span_iterator& {
-        ptr += n;
+        ptr_ += n;
         return *this;
     }
     auto operator-=(difference_type n) -> span_iterator& {
-        ptr -= n;
+        ptr_ -= n;
         return *this;
     }
 
     // Comparison
-    auto operator==(span_iterator const& other) const -> bool { return ptr == other.ptr; }
-    auto operator!=(span_iterator const& other) const -> bool { return ptr != other.ptr; }
+    auto operator==(span_iterator const& other) const -> bool { return ptr_ == other.ptr_; }
+    auto operator!=(span_iterator const& other) const -> bool { return ptr_ != other.ptr_; }
     auto operator<=>(span_iterator const&) const = default;
+  private:
+    T* ptr_{nullptr};
 };
 
 static_assert(std::input_or_output_iterator<ml::span_iterator<int>>);
