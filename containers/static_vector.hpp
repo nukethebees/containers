681f013b28a9788fffa8103d491c43985e81b3c2
Matthew Love <matthewlove94@gmail.com>
2025-05-01 14:21:25 +0100

    Added a destructor

--- containers/static_vector.hpp
+++ containers/static_vector.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 
 #include "span_iterator.hpp"
@@ -7,6 +8,12 @@
 namespace ml {
 template <typename T, std::size_t CAPACITY>
 class static_vector {
+  private:
+    union array_elem_t {
+        T value;
+
+        ~array_elem_t() {}
+    };
   public:
     using value_type = T;
     using size_type = std::size_t;
@@ -20,11 +27,22 @@ class static_vector {
     using reverse_iterator = std::reverse_iterator<iterator>;
     using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 
+    static_vector() = default;
+    ~static_vector();
+
     constexpr auto capacity() const -> size_type;
     auto size() const -> size_type;
   private:
+    std::array<array_elem_t, CAPACITY> elements_;
     size_type size_{0};
 };
+
+template <typename T, std::size_t CAPACITY>
+inline static_vector<T, CAPACITY>::~static_vector() {
+    for (size_type i{0}; i < size_; ++i) {
+        elements_[i].value.~T();
+    }
+}
 
 template <typename T, std::size_t CAPACITY>
 inline constexpr auto static_vector<T, CAPACITY>::capacity() const -> size_type {
