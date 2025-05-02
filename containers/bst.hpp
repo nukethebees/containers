474e260d59dfdd68f90c14110562899ac80935fa
Matthew Love <matthewlove94@gmail.com>
2025-05-02 12:10:39 +0100

    Added insertion and removal

--- containers/bst.hpp
+++ containers/bst.hpp
@@ -10,40 +10,165 @@
 namespace ml {
 namespace detail {
 template <typename T>
-class bst_node {};
-}
-
-template <typename T, typename Compare = std::less<T>, typename Allocator = std::allocator<T>>
-class bst {
+class bst_node {
   public:
     using value_type = T;
-    using size_type = std::size_t;
-    using allocator_type = Allocator;
     using reference = value_type&;
     using const_reference = value_type const&;
     using pointer = value_type*;
     using const_pointer = value_type const*;
-    using node_type = detail::bst_node<T>;
 
+    bst_node() = delete;
+    bst_node(T&& value) noexcept;
+
+    ~bst_node() = default;
+
+    auto value() -> reference;
+    auto value() const -> const_reference;
+    auto less() -> bst_node*;
+    auto less() const -> bst_node const*;
+    auto greater() -> bst_node*;
+    auto greater() const -> bst_node const*;
+
+    auto operator*() -> reference;
+    auto operator*() const -> const_reference;
+  private:
+    T value_;
+    bst_node* less_{nullptr};
+    bst_node* greater_{nullptr};
+};
+
+template <typename T>
+bst_node<T>::bst_node(T&& value) noexcept
+    : value_{std::move(value)} {}
+
+#define METHOD_START      \
+    template <typename T> \
+    inline auto bst_node<T>
+
+METHOD_START::value()->reference {
+    return value_;
+}
+METHOD_START::value() const->const_reference {
+    return value_;
+}
+METHOD_START::less()->bst_node* {
+    return less_;
+}
+METHOD_START::less() const->bst_node const* {
+    return less_;
+}
+METHOD_START::greater()->bst_node* {
+    return greater_;
+}
+METHOD_START::greater() const->bst_node const* {
+    return greater_;
+}
+
+METHOD_START::operator*()->reference {
+    return value_;
+}
+METHOD_START::operator*() const->const_reference {
+    return value_;
+}
+
+#undef METHOD_START
+}
+
+template <typename T, typename Compare = std::less<T>, template <typename> typename Allocator = std::allocator>
+class bst {
+  public:
+    using value_type = T;
+    using node_type = detail::bst_node<value_type>;
+    using size_type = std::size_t;
+    using allocator_type = Allocator<node_type>;
+    using reference = value_type&;
+    using const_reference = value_type const&;
+    using pointer = value_type*;
+    using const_pointer = value_type const*;
+
+    // Access
+    auto root() -> pointer;
+    auto root() const -> const_pointer;
     // Capacity
     auto empty() const -> bool;
     auto size() const -> bool;
+    // Modifiers
+    void clear();
+    template <typename U>
+    void insert(U&& value);
+    // Caller needs to nullify the node afterwards
+    void remove(node_type* node);
   private:
     inline static Compare compare{};
-    NO_UNIQUE_ADDRESS Allocator alloc_;
+    NO_UNIQUE_ADDRESS allocator_type alloc_;
     node_type* root_{nullptr};
     size_type size_{0};
 };
 
-#define METHOD_START                                            \
-    template <typename T, typename Compare, typename Allocator> \
+#define METHOD_START(...)                                                           \
+    template <typename T, typename Compare, template <typename> typename Allocator> \
+    __VA_OPT__(__VA_ARGS__)                                                         \
     inline auto bst<T, Compare, Allocator>
 
-METHOD_START::empty() const->bool {
+METHOD_START()::root()->pointer {
+    if (!root_) {
+        return nullptr;
+    }
+    return &root_->value();
+}
+METHOD_START()::root() const->const_pointer {
+    if (!root_) {
+        return nullptr;
+    }
+    return &root_->value();
+}
+
+METHOD_START()::empty() const->bool {
     return size_ == 0;
 }
-METHOD_START::size() const->bool {
+METHOD_START()::size() const->bool {
     return size_;
+}
+
+METHOD_START()::clear()->void {
+    if (root_) {
+        remove(root_);
+        root_ = nullptr;
+    }
+}
+METHOD_START(template <typename U>)::insert(U&& new_value)->void {
+    auto* current{root_};
+    while (true) {
+        if (!current) {
+            current = alloc_.allocate(1);
+            new (current) node_type(std::forward<U>(new_value));
+            size_++;
+            return;
+        }
+
+        auto const& node_value{current->value()};
+        if (compare(new_value, node_value)) {
+            current = current->less();
+        } else if (compare(node_value, new_value)) {
+            current = current->greater();
+        } else {
+            return;
+        }
+    }
+}
+METHOD_START()::remove(node_type* node)->void {
+    if (!node) {
+        return;
+    }
+    if (node->less()) {
+        remove(node->less());
+    }
+    if (node->greater()) {
+        remove(node->greater());
+    }
+    alloc_.deallocate(node, 1);
+    size_--;
 }
 
 #undef METHOD_START
--- tests/test_bst.cpp
+++ tests/test_bst.cpp
@@ -8,3 +8,14 @@ TEST(bst, empty_bst) {
     ml::bst<int> bst;
     ASSERT_TRUE(bst.empty());
 }
+TEST(bst, insert_single) {
+    ml::bst<int> bst;
+    bst.insert(1);
+    ASSERT_EQ(bst.size(), 1);
+}
+TEST(bst, remove_single_verify_null_root) {
+    ml::bst<int> bst;
+    bst.insert(1);
+    bst.clear();
+    ASSERT_EQ(nullptr, bst.root());
+}
