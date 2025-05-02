8764736407507d9cc65e93b69f954fa73ad491f8
Matthew Love <matthewlove94@gmail.com>
2025-05-02 11:20:07 +0100

    Added size and compare members

--- containers/bst.hpp
+++ containers/bst.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstddef>
+#include <functional>
 #include <memory>
 
 #include "platform_def.hpp"
@@ -11,26 +13,37 @@ template <typename T>
 class bst_node {};
 }
 
-template <typename T, typename Allocator = std::allocator<T>>
+template <typename T, typename Compare = std::less<T>, typename Allocator = std::allocator<T>>
 class bst {
   public:
     using value_type = T;
-    using node_type = detail::bst_node<T>;
+    using size_type = std::size_t;
     using allocator_type = Allocator;
+    using reference = value_type&;
+    using const_reference = value_type const&;
+    using pointer = value_type*;
+    using const_pointer = value_type const*;
+    using node_type = detail::bst_node<T>;
 
     // Capacity
     auto empty() const -> bool;
+    auto size() const -> bool;
   private:
+    inline static Compare compare{};
     NO_UNIQUE_ADDRESS Allocator alloc_;
     node_type* root_{nullptr};
+    size_type size_{0};
 };
 
-#define METHOD_START                          \
-    template <typename T, typename Allocator> \
-    inline auto bst<T, Allocator>
+#define METHOD_START                                            \
+    template <typename T, typename Compare, typename Allocator> \
+    inline auto bst<T, Compare, Allocator>
 
 METHOD_START::empty() const->bool {
-    return true;
+    return size_ == 0;
+}
+METHOD_START::size() const->bool {
+    return size_;
 }
 
 #undef METHOD_START
