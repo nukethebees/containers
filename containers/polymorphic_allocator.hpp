55c32a86240f915e04bb24f53c5478fed82aea43
Matthew Love <matthewlove94@gmail.com>
2025-05-14 16:20:40 +0100

    Added extension to polymorphic_allocator

--- containers/polymorphic_allocator.hpp
+++ containers/polymorphic_allocator.hpp
@@ -4,6 +4,7 @@
 #include <compare>
 #include <memory_resource>
 
+#include "allocator_concepts.hpp"
 #include "memory_resource.hpp"
 
 namespace ml {
@@ -51,6 +52,16 @@ class polymorphic_allocator {
         }
     }
     void deallocate_bytes(void* p, size_type n, size_type alignment) { resource_->deallocate(p, n, alignment); }
+    // Extension
+    auto extend(T* ptr, size_type old_elems, size_type new_elems)
+        -> void* requires(extendable_memory_resource<ResourceT>) {
+            return static_cast<T*>(resource_->extend(ptr, old_elems * sizeof(T), new_elems * sizeof(T), alignof(T)));
+        }
+
+    auto extend_bytes(void* ptr, size_type old_bytes, size_type new_bytes, size_type alignment)
+        -> void* requires(extendable_memory_resource<ResourceT>) {
+            return resource_->extend(ptr, old_bytes, new_bytes, alignment);
+        }
 
     // Object construction/destruction
     template <class U, class... Args>
