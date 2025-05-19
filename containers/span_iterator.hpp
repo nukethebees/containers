22e893cbf1d7f7e64682219f3e72afed740a0d0e
Matthew Love <matthewlove94@gmail.com>
2025-05-19 16:44:34 +0100

    Marked many methods as noexcept

--- containers/span_iterator.hpp
+++ containers/span_iterator.hpp
@@ -15,67 +15,67 @@ class span_iterator {
     using iterator_category = std::contiguous_iterator_tag;
 
     // Needed to satisfy the default constructible requirement
-    span_iterator() = default;
-    explicit span_iterator(T* ptr)
+    span_iterator() noexcept = default;
+    explicit span_iterator(T* ptr) noexcept
         : ptr_(ptr) {}
 
-    span_iterator(span_iterator const&) = default;
-    span_iterator(span_iterator&&) = default;
+    span_iterator(span_iterator const&) noexcept = default;
+    span_iterator(span_iterator&&) noexcept = default;
 
-    auto operator=(span_iterator const&) -> span_iterator& = default;
-    auto operator=(span_iterator&&) -> span_iterator& = default;
+    auto operator=(span_iterator const&) noexcept -> span_iterator& = default;
+    auto operator=(span_iterator&&) noexcept -> span_iterator& = default;
 
     // Dereferencing
     // For some reason, a const iterator needs to return T& to meet the iter requirements
-    auto operator*() const -> reference { return *ptr_; }
-    auto operator*() -> reference { return *ptr_; }
-    auto operator->() const -> T* { return ptr_; }
-    auto operator[](difference_type n) const -> reference { return *(ptr_ + n); }
+    auto operator*() const noexcept -> reference { return *ptr_; }
+    auto operator*() noexcept -> reference { return *ptr_; }
+    auto operator->() const noexcept -> T* { return ptr_; }
+    auto operator[](difference_type n) const noexcept -> reference { return *(ptr_ + n); }
 
     // Increment / decrement
-    auto operator++() -> span_iterator& {
+    auto operator++() noexcept -> span_iterator& {
         ++ptr_;
         return *this;
     }
-    auto operator++(int) -> span_iterator {
+    auto operator++(int) noexcept -> span_iterator {
         auto temp{*this};
         ++(*this);
         return temp;
     }
 
-    auto operator--() -> span_iterator& {
+    auto operator--() noexcept -> span_iterator& {
         --ptr_;
         return *this;
     }
-    auto operator--(int) -> span_iterator {
+    auto operator--(int) noexcept -> span_iterator {
         auto temp{*this};
         --(*this);
         return temp;
     }
 
     // Math operators
-    auto operator+(difference_type n) const -> span_iterator { return span_iterator(ptr_ + n); }
+    auto operator+(difference_type n) const noexcept -> span_iterator { return span_iterator(ptr_ + n); }
     friend auto operator+(difference_type lhs, span_iterator const& rhs) -> span_iterator {
         return span_iterator(lhs + rhs.ptr_);
     }
-    auto operator-(difference_type n) const -> span_iterator { return span_iterator(ptr_ - n); }
+    auto operator-(difference_type n) const noexcept -> span_iterator { return span_iterator(ptr_ - n); }
     friend auto operator-(difference_type lhs, span_iterator const& rhs) -> span_iterator {
         return span_iterator(lhs - rhs.ptr_);
     }
-    auto operator-(span_iterator const& n) const -> difference_type { return difference_type{ptr_ - n.ptr_}; }
+    auto operator-(span_iterator const& n) const noexcept -> difference_type { return difference_type{ptr_ - n.ptr_}; }
     auto operator+=(difference_type n) -> span_iterator& {
         ptr_ += n;
         return *this;
     }
-    auto operator-=(difference_type n) -> span_iterator& {
+    auto operator-=(difference_type n) noexcept -> span_iterator& {
         ptr_ -= n;
         return *this;
     }
 
     // Comparison
-    auto operator==(span_iterator const& other) const -> bool { return ptr_ == other.ptr_; }
-    auto operator!=(span_iterator const& other) const -> bool { return ptr_ != other.ptr_; }
-    auto operator<=>(span_iterator const&) const = default;
+    auto operator==(span_iterator const& other) const noexcept -> bool { return ptr_ == other.ptr_; }
+    auto operator!=(span_iterator const& other) const noexcept -> bool { return ptr_ != other.ptr_; }
+    auto operator<=>(span_iterator const&) const noexcept = default;
   private:
     T* ptr_{nullptr};
 };
