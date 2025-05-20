0a85ac50206f0660b609ce306c39f74d54a445d7
Matthew Love <matthewlove94@gmail.com>
2025-05-20 21:43:59 +0100

    implemented bubble_sort

---
+++ containers/bubble_sort.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iterator>
+
+namespace ml {
+template <typename Iterator>
+    requires std::bidirectional_iterator<Iterator>
+void bubble_sort(Iterator begin, Iterator end) {
+    bool is_sorted = false;
+    while (!is_sorted) {
+        is_sorted = true;
+
+        for (auto it = begin; it != std::prev(end); ++it) {
+            auto next_it = std::next(it);
+
+            if (*it > *next_it) {
+                std::swap(*it, *next_it);
+                is_sorted = false;
+            }
+        }
+    }
+}
+}
