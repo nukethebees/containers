ef26b7ebdcaa478187aff49bbf90259f30384976
Matthew Love <matthewlove94@gmail.com>
2025-05-20 22:02:11 +0100

    Made a function template for a simple sorting test

---
+++ tests/test_sort.cpp
@@ -0,0 +1,24 @@
+#include <array>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "containers/bubble_sort.hpp"
+
+template <typename T>
+class SortTest : public testing::Test {};
+
+template <auto sort_fn>
+void test_simple_int_sort() {
+    std::array<int, 5> v{5, 4, 3, 2, 1};
+    std::array<int, 5> v_exp{1, 2, 3, 4, 5};
+
+    sort_fn(v.begin(), v.end());
+    EXPECT_EQ(v, v_exp);
+}
+
+#define WRAP_FN(FN) [](auto&&... args) { return FN(std::forward<decltype(args)>(args)...); }
+
+TEST(bubble_sort, simple_ints) {
+    test_simple_int_sort<WRAP_FN(ml::bubble_sort)>();
+}
