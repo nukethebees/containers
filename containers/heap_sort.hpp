1d29b781036d034fcf4cef389df0a88c286b3391
Matthew Love <matthewlove94@gmail.com>
2025-05-22 15:58:35 +0100

    heap sort implemented

--- containers/heap_sort.hpp
+++ containers/heap_sort.hpp
@@ -3,7 +3,21 @@
 #include <iterator>
 #include <utility>
 
+#include "binary_heap.hpp"
+
 namespace ml {
 template <typename Iterator>
-void heap_sort(Iterator begin, Iterator end) {}
+void heap_sort(Iterator begin, Iterator end) {
+    binary_heap<typename Iterator::value_type> heap;
+    heap.reserve(static_cast<std::size_t>(end - begin));
+
+    for (auto it{begin}; it != end; ++it) {
+        heap.insert(*it);
+    }
+
+    while (!heap.empty()) {
+        *begin = heap.pop();
+        ++begin;
+    }
+}
 }
